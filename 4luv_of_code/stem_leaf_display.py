64aacef36d611847f0fb3c555e968476fc2a905d
stelios-c <stelios.christodoulou@yahoo.com>
2025-09-06 20:37:50 +0100

    use the module after importing

--- 4luv_of_code/stem_leaf_display.py
+++ 4luv_of_code/stem_leaf_display.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
 import argparse
+
+parser = argparse.ArgumentParser(
+        prog ='ProgramName',
+        description='A script that reads a dataset from a file and outputs a stem and leaf display',
+        epilog='submission to "For the Love of Code 2025"')
+
+parser.add_argument('filename')
